a5a4d344cc1660eaf63b508baa605c78da6aa505
k74 <kashhh4u@gmail.com>
2023-07-22 21:28:58 +0100

    Add styling

--- src/components/FutureWeather.tsx
+++ src/components/FutureWeather.tsx
@@ -11,12 +11,12 @@ interface FutureWeatherProps {
 
 export default function FutureWeather ({date, tempMax, tempMin, icon, conditions} : FutureWeatherProps) {
     return(
-        <div className="weather">
-            <h1>{convertDatetoDay(date)}</h1>
-            <img src={icon} alt={conditions} />
+        <div className="future-weather">
+            <h2 className="day">{convertDatetoDay(date)}</h2>
+            <img src={`src/assets/icons/${icon}.svg`} alt={conditions} />
             <div className="temps">
-                <div className="temp high">{tempMax}</div>
-                <div className="temp low">{tempMin}</div>
+                <div className="temp high">{tempMax}&#176;</div>
+                <div className="temp low">{tempMin}&#176;</div>
             </div>
         </div>
     )
--- src/components/WeatherContainer.tsx
+++ src/components/WeatherContainer.tsx
@@ -10,7 +10,7 @@ export default function WeatherContainer () {
     const getWeatherData = async (location : string) => {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${import.meta.env.VITE_API_KEY}&unitGroup=uk`)
         const data = await response.json();
-        setWeatherData(data);
+        setWeatherData(data.currentConditions);
         setFutureWeatherData(data.days.slice(1,6))
         setLoad(false);
         console.log(data);
@@ -21,21 +21,34 @@ export default function WeatherContainer () {
     }, [])    
     return (
         <div className="weather-container">
-            <img className='curr-weather-icon' src="" alt={load ? "loading" :weatherData.currentConditions.conditions} />
+            <div className="current-weather-icon">
+                <img src={load ? `none` :`src/assets/icons/${weatherData.icon}.svg`} alt={load ? "loading" :weatherData.conditions} />
+            </div>
             <div className="current-weather">
                 <div className="left-container">
-                    <h1 data-testid="location">{location}</h1>
-                    <h2>Hourly</h2>
+                    <h2 data-testid="location" className='location'>{location}</h2>
+                    <h3 className="hourly">Hourly</h3>
                 </div>
                 <div className="right-container">
                     <div className="degrees">
-                        { load ? (<h1>Loading</h1>) : weatherData.currentConditions.temp}
+                        { load ? (<h1>Loading</h1>) : Math.floor(weatherData.temp)}<span className='degrees-symbol'>&#176;</span> 
                     </div>
                 </div>
             </div>
+            <div className="future-weather-container">
             { 
-                load ? (<h1>Loading</h1>) : futureWeatherData.map((day : any) => <FutureWeather date={day.datetime} tempMax={day.tempmax} tempMin={day.tempmin} icon={day.icon} conditions={day.conditions}/>)
+                load ? (<h2>Loading</h2>) : futureWeatherData.map((day : any) => <FutureWeather date={day.datetime} tempMax={day.tempmax} tempMin={day.tempmin} icon={day.icon} conditions={day.conditions}/>)
             }
+            </div>
+            <div className="last-fetched">
+                <a href="https://www.visualcrossing.com/">
+                    <img className='visual-crossing' src="src/assets/icons/Visual_Crossing_Corporation_Logo.png" alt="Visual Crossing Logo" />
+                </a>
+                <div className="upated"><strong>Updated</strong> {new Date().toLocaleDateString('en-GB', { hour: 'numeric', minute: 'numeric', hour12: true })}</div>
+                <a href="https://github.com/Bsodoge/Weather-App">
+                    <img src="src/assets/icons/github-mark.svg" alt="Github Logo" />
+                </a>
+            </div>
         </div>
     )
 }
