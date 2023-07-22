5a93a21b8c6ed76f2c8010946d7aba278713798f
k74 <kashhh4u@gmail.com>
2023-07-22 16:44:01 +0100

    Add structure for container

--- src/components/WeatherContainer.tsx
+++ src/components/WeatherContainer.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { useState, useEffect } from "react";
+import FutureWeather from './FutureWeather';
 
 export default function WeatherContainer () {
-    let [weatherData, setWeatherData] = useState<any>([]);
+    let [weatherData, setWeatherData] = useState<any>();
+    let [futureWeatherData, setFutureWeatherData] = useState<any>();
     let [load, setLoad] = useState<boolean>(true);
     let [location, setLocation] = useState<string>("London")
     const getWeatherData = async (location : string) => {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${import.meta.env.VITE_API_KEY}&unitGroup=uk`)
         const data = await response.json();
         setWeatherData(data);
+        setFutureWeatherData(data.days.slice(1,6))
         setLoad(false);
+        console.log(data);
     }
 
     useEffect(() => {
@@ -17,6 +21,7 @@ export default function WeatherContainer () {
     }, [])    
     return (
         <div className="weather-container">
+            <img className='curr-weather-icon' src="" alt={load ? "loading" :weatherData.currentConditions.conditions} />
             <div className="current-weather">
                 <div className="left-container">
                     <h1 data-testid="location">{location}</h1>
@@ -28,6 +33,9 @@ export default function WeatherContainer () {
                     </div>
                 </div>
             </div>
+            { 
+                load ? (<h1>Loading</h1>) : futureWeatherData.map((day : any) => <FutureWeather date={day.datetime} tempMax={day.tempmax} tempMin={day.tempmin} icon={day.icon} conditions={day.conditions}/>)
+            }
         </div>
     )
 }
