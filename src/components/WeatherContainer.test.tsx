3376e45eb5ab0034ae957ee3d3005a5390030228
k74 <kashhh4u@gmail.com>
2023-07-24 12:38:50 +0100

    Add ability to edit location

--- src/components/WeatherContainer.test.tsx
+++ src/components/WeatherContainer.test.tsx
@@ -1,3 +1,4 @@
+import { enableFetchMocks } from 'jest-fetch-mock'
 import React from 'react';
 import { render } from "@testing-library/react";
 import WeatherContainer from "./WeatherContainer";
@@ -5,6 +6,7 @@ import WeatherContainer from "./WeatherContainer";
 
 describe(WeatherContainer, () => {
     it("Title displays the correct initial value",() => {
+        enableFetchMocks()
         const {getByTestId} = render(<WeatherContainer/>);
         const locationValue = getByTestId("location").textContent;
         expect(locationValue).toEqual("London")
--- src/components/WeatherContainer.tsx
+++ src/components/WeatherContainer.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { ReactElement, useRef } from 'react';
 import { useState, useEffect } from "react";
 import FutureWeather from './FutureWeather';
 
 export default function WeatherContainer () {
-    let [weatherData, setWeatherData] = useState<any>();
-    let [futureWeatherData, setFutureWeatherData] = useState<any>();
-    let [load, setLoad] = useState<boolean>(true);
-    let [location, setLocation] = useState<string>("London")
-    const getWeatherData = async (location : string) => {
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${import.meta.env.VITE_API_KEY}&unitGroup=uk`)
-        const data = await response.json();
-        setWeatherData(data.currentConditions);
-        setFutureWeatherData(data.days.slice(1,6))
-        setLoad(false);
-        console.log(data);
+    const [weatherData, setWeatherData] = useState<any>();
+    const [futureWeatherData, setFutureWeatherData] = useState<any>();
+    const [load, setLoad] = useState<boolean>(true);
+    const [location, setLocation] = useState<string>("London");
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const editBox = useRef<HTMLInputElement | null>(null)
+    const toggleEditMode = () => {
+        setEditMode(edit => !edit);
     }
-
+    const getWeatherData = async (location : string) => {
+        try {
+            const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${import.meta.env.VITE_API_KEY}&unitGroup=uk`)
+            const data = await response.json();
+            setLocation(data.address.toLowerCase());
+            setWeatherData(data.currentConditions);
+            setFutureWeatherData(data.days.slice(1,6))
+            setLoad(false);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    const changeLocation = (location : string) =>{
+        if((location || location.trim().length)){
+            toggleEditMode();
+            getWeatherData(location);
+        }
+    }
     useEffect(() => {
       getWeatherData(location);
     }, [])    
@@ -26,7 +41,7 @@ export default function WeatherContainer () {
             </div>
             <div className="current-weather">
                 <div className="left-container">
-                    <h2 data-testid="location" className='location'>{location}</h2>
+                    { editMode ? <input className='edit' ref={editBox} defaultValue={location} onKeyDown={(e) => {if(e.key === 'Enter') changeLocation(editBox.current!.value)}}></input> : <h2 data-testid="location" className='location' onDoubleClick={toggleEditMode}>{location}</h2>}
                     <h3 className="hourly">Hourly</h3>
                 </div>
                 <div className="right-container">
